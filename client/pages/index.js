8a982adaa4eaae43d63d8535f115262d0ba7209e
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-02 19:08:27 -0400

    currentUser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,23 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (process.browser) {
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  } else {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  }
+};
+
+export default LandingPage;
