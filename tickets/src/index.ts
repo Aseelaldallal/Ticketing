83b9b2f50de7ee4732f58b55dcd3d59cacea5606
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-03 18:33:16 -0400

    tickets service backbone

---
+++ tickets/src/index.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  try {
+    console.log('Attempting to connect to db...');
+    await mongoose.connect('mongodb://auth-mongo-service:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to Mongo DB');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on 3000');
+  });
+};
+
+start();
