3f56497be78a47cf05aca27f5313dd9c8131a1c2
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-05 13:10:11 -0400

    create ticket route

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@gettix/common';
+import { Ticket } from '../models/tickets';
 
 const router = express.Router();
 
@@ -12,8 +13,16 @@ router.post(
     body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const userId = req.currentUser!.id;
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId,
+    });
+    await ticket.save();
+    res.sendStatus(201).send(ticket);
   }
 );
 
