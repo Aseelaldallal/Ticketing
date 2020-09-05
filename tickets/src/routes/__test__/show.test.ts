9d5c661d38268428708b43230094da0a7f5add82
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-05 15:02:09 -0400

    test with fail

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 404 if the ticket is not found', async () => {
+  const resp = await request(app).get('/api/tickets/12345');
+});
+
+it('returns the ticket if the ticket is found', async () => {
+  const title = 'concert';
+  const price = 20;
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title,
+      price,
+    })
+    .expect(201);
+  const ticketResponse = await request(app).get(`/api/tickets/${response.body.id}`).send().expect(200);
+  expect(ticketResponse.body.title).toEqual(title);
+  expect(ticketResponse.body.price).toEqual(price);
+});
