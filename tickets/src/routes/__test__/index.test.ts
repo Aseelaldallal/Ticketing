7edd0c152f776eb4e867de24a298ba15c8a801d0
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-05 16:01:30 -0400

    index route

---
+++ tickets/src/routes/__test__/index.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/tickets';
+
+const createTicket = () => {
+  return request(app).post('/api/tickets').set('Cookie', global.signup()).send({
+    title: 'Valid title',
+    price: 20,
+  });
+};
+
+it('can fetch a list of tickets', async () => {
+  await createTicket();
+  await createTicket();
+  await createTicket();
+  const response = await request(app).get('/api/tickets').send().expect(200);
+  expect(response.body.length).toEqual(3);
+});
