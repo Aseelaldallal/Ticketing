601f865805d13f19611e36381180afeb60ed2ceb
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-21 19:57:02 -0400

    add validation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@gettix/common';
+import { requireAuth, validateRequest } from '@gettix/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', requireAuth, async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [body('ticketId').not().isEmpty().withMessage('TicketId must be provided')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
