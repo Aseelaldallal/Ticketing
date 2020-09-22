be7899f64f426cd5459fa612929193a321bb51b6
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-22 17:48:51 -0400

    create models

---
+++ orders/src/models/orders.ts
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@gettix/common';
+import { TicketDoc } from './ticket';
+
+// An interface that describes the properties that
+// are required to create a new Order
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+  ß;
+}
+
+// An inteface that describes the properties that a Order
+// Document has
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties that the
+// Order model has
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+      // not required. if someone pays for order, we don't want to expire it
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', OrderSchema);
+
+export { Order };
