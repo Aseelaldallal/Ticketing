ab1ac0cbecae83fcd696527d49f59317275a8796
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-27 17:15:14 -0400

    ticket updated listener

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -7,7 +7,18 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-    const ticket = await Ticket.findById(data.id);
+    // console.log('DATA', data);
+    // const ticket1 = await Ticket.findById(data.id);
+    // console.log('Ticket1', ticket1);
+    // const ticket2 = await Ticket.findOne({
+    //   version: data.version - 1,
+    // });
+    // console.log('Ticket2', ticket2);
+    const ticket = await Ticket.findOne({
+      _id: data.id,
+      version: data.version - 1,
+    });
+    console.log('My ticket', ticket);
     if (!ticket) {
       throw new Error('Ticket not found');
     }
