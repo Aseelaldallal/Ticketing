1a9f57513caeb2b8cafec3fb3dbb4db8a1913503
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-09-27 17:15:59 -0400

    take version into account

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -7,13 +7,6 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   readonly subject = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-    // console.log('DATA', data);
-    // const ticket1 = await Ticket.findById(data.id);
-    // console.log('Ticket1', ticket1);
-    // const ticket2 = await Ticket.findOne({
-    //   version: data.version - 1,
-    // });
-    // console.log('Ticket2', ticket2);
     const ticket = await Ticket.findOne({
       _id: data.id,
       version: data.version - 1,
