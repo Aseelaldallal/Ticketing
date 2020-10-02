7b353722367d4220333b9b86b2218f25c74700d3
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-10-02 19:09:19 -0400

    enqueue job

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -2,10 +2,16 @@ import { Listener } from '@gettix/common/build/events/base-listener';
 import { OrderCreatedEvent } from '@gettix/common/build/events/types/order-created-event';
 import { Subjects } from '@gettix/common/build/events/types/subjects';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
 import { queueGroupName } from './queue-group-name';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+    msg.ack();
+  }
 }
