630617aa0661aa704e5bd8e40fc9c5b11de8b516
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-10-02 18:50:34 -0400

    order created listener backbone

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,11 @@
+import { Listener } from '@gettix/common/build/events/base-listener';
+import { OrderCreatedEvent } from '@gettix/common/build/events/types/order-created-event';
+import { Subjects } from '@gettix/common/build/events/types/subjects';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+}
