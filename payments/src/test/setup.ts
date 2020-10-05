8dead61d84aac4bf341deaee8819e8bf559e2688
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-10-05 19:19:03 -0400

    rename to singin, add option userId as arg

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 declare global {
   namespace NodeJS {
     interface Global {
-      signup: () => string[];
+      signin: (id?: string) => string[];
     }
   }
 }
@@ -39,10 +39,10 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signup = () => {
+global.signin = (id?: string) => {
   // Build a jsonwebtoken payload {id, email}
   const payload = {
-    id: new mongoose.Types.ObjectId().toHexString(),
+    id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
   // Create the JWT
