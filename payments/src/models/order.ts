ccaaecf65bdd8394d753d3705db395a8029fa999
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-10-05 17:56:11 -0400

    create file

---
+++ payments/src/models/order.ts
@@ -0,0 +1,66 @@
+import { OrderStatus } from '@gettix/common';
+import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+// An interface that describes the properties that
+// are required to create a new Order
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+// An inteface that describes the properties that a Order
+// Document has
+interface OrderDoc extends mongoose.Document {
+  // no need to add ID, mongoose.Document has ID
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+// An interface that describes the properties that the
+// Order model has
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+OrderSchema.set('versionKey', 'version');
+OrderSchema.plugin(updateIfCurrentPlugin);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({ _id: attrs.id, ...attrs });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', OrderSchema);
+
+export { Order };
