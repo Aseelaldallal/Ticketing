2b7500212e1573395e4cb4e851343755b2712a03
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-10-06 17:17:43 -0400

    save charge

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -10,6 +10,7 @@ import {
 import { body } from 'express-validator';
 import { Order } from '../models/order';
 import { stripe } from '../stripe';
+import { Payment } from '../models/payment';
 
 const router = express.Router();
 
@@ -30,11 +31,19 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order');
     }
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
       currency: 'USD',
       amount: order.price * 100,
       source: token,
     });
+
+    const payment = Payment.build({
+      orderId: order.id,
+      stripeId: charge.id,
+    });
+
+    await payment.save();
+
     res.status(201).send({ success: true });
   }
 );
