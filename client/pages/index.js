b8d6f5cc3ab8a8144fc729d14edd5788078ae3d3
Aseel Al Dallal <aseelaldallal@gmail.com>
2020-10-07 17:02:51 -0400

    list tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,32 @@
 const LandingPage = ({ currentUser, tickets }) => {
-  console.log('Tickets', tickets);
-  return currentUser ? <h1> You are signed in </h1> : <h1> You are not signed in</h1>;
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
-  console.log('hello');
   const { data } = await client.get('/api/tickets');
-  console.log('Data', data);
+
   return { tickets: data };
 };
 
